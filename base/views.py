e8644e8dd7df5579abbcfa02084b09269285b5b1
MD. SHOFIULLAH <41083322+mdshofiullah@users.noreply.github.com>
2021-12-31 03:01:30 +0600

    Deleting comment from room

--- base/views.py
+++ base/views.py
@@ -149,3 +149,16 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You can not allow here!!!!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
