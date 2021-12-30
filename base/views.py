bef9b677249f1833a6a7b85ed5d7e7e65aefccc6
MD. SHOFIULLAH <41083322+mdshofiullah@users.noreply.github.com>
2021-12-31 02:52:55 +0600

    add new participants on room or topic

--- base/views.py
+++ base/views.py
@@ -98,6 +98,7 @@ def room(request, pk):
             room=room,
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
     context = {'room': room, 'room_messages': room_messages,
