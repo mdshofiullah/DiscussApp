4b37323915db80b2e8a20547d741626e8a0c20be
MD. SHOFIULLAH <41083322+mdshofiullah@users.noreply.github.com>
2021-12-31 02:33:34 +0600

    Commenting on a post/room

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Message, Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
@@ -87,10 +87,18 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
 
-    room_messages = room.message_set.all()
+    room_messages = room.message_set.all().order_by('-created')
     # for i in rooms:
     #     if i['id'] == int(pk):
     #         room = i
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+
     context = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html', context)
 
