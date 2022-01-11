09fae729e1e3935b86043580b6ac7d9dfce3e02d
MD. SHOFIULLAH <41083322+mdshofiullah@users.noreply.github.com>
2022-01-12 03:26:00 +0600

    template first upload

--- base/views.py
+++ base/views.py
@@ -133,7 +133,9 @@ def createRoom(request):
     if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect("home")
         # print(request.post)
     context = {"form": form}
