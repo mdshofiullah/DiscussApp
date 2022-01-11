7a0124fa8236865e44b48bacb9173999095c094c
MD. SHOFIULLAH <41083322+mdshofiullah@users.noreply.github.com>
2022-01-12 03:03:48 +0600

    room from first update

--- base/forms.py
+++ base/forms.py
@@ -7,4 +7,5 @@ from .models import Room
 class RoomForm(ModelForm):
     class Meta:
         model = Room
-        fields = '__all__'
+        fields = "__all__"
+        exclude = ["host", "participants"]
