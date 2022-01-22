2d631f09796fc080648f29dfe4ac298e164976b0
mdshofiullah <mdshofiullah65@gmail.com>
2022-01-22 14:19:37 +0600

    new method to custimize room form value and get it to frontend

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from os import name
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
@@ -132,12 +133,20 @@ def createRoom(request):
     form = RoomForm()
     topics = Topic.objects.all()
     if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect("home")
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
+        # form = RoomForm(request.POST)
+        # if form.is_valid():
+        #     room = form.save(commit=False)
+        #     room.host = request.user
+        #     room.save()
+        return redirect("home")
         # print(request.post)
     context = {"form": form, 'topics': topics}
     return render(request, "base/room_form.html", context)
